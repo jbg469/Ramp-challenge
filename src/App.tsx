cd416277784123bb5b6ede36a41cbd0eca1298fa
jbg469 <72175659+jbg469@users.noreply.github.com>
2022-10-08 12:12:22 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,127 @@ export function App() {
   const loadAllTransactions = useCallback(async () => {
     setIsLoading(true)
     transactionsByEmployeeUtils.invalidateData()
+import { Fragment, useCallback, useEffect, useMemo, useState } from "react"
+import { Employee } from "./utils/types"
+import { InputSelect } from "./components/InputSelect"
+import { TransactionPane } from "./components/TransactionPane"
+import { Instructions } from "./components/Instructions"
+import { useEmployees } from "./hooks/useEmployees"
+import { usePaginatedTransactions } from "./hooks/usePaginatedTransactions"
+import { useTransactionsByEmployee } from "./hooks/useTransactionsByEmployee"
+import { EMPTY_EMPLOYEE } from "./utils/constants"
+
+
+export function App() {
+  const { data: employees, ...employeeUtils } = useEmployees()
+  const { data: paginatedTransactions, ...paginatedTransactionsUtils } = usePaginatedTransactions()
+  const { data: transactionsByEmployee, ...transactionsByEmployeeUtils } = useTransactionsByEmployee()
+  const [isLoading, setIsLoading] = useState(false)
+  const [isPaginated, setIsPaginated] = useState(false)
+  
+
+  const transactions = useMemo(
+    () => paginatedTransactions?.data ?? transactionsByEmployee ?? null,
+    [paginatedTransactions, transactionsByEmployee]
+  )
+
+  const loadAllTransactions = useCallback(async () => {
+    
+    setIsLoading(true)
+    setIsPaginated(false)
+    transactionsByEmployeeUtils.invalidateData()
+
+    await employeeUtils.fetchAll()
+    await paginatedTransactionsUtils.fetchAll()
+
+    setIsLoading(false)
+  }, [employeeUtils, paginatedTransactionsUtils, transactionsByEmployeeUtils])
+
+  const loadTransactionsByEmployee = useCallback(
+    async (employeeId: string) => {
+      setIsPaginated(true)
+      paginatedTransactionsUtils.invalidateData()
+      await transactionsByEmployeeUtils.fetchById(employeeId)
+    },
+    [paginatedTransactionsUtils, transactionsByEmployeeUtils]
+  )
+
+ 
+  useEffect(() => {
+    if (employees === null && !employeeUtils.loading) {
+      loadAllTransactions();
+      
+    }
+  }, [employeeUtils.loading, employees, loadAllTransactions])
+
+  return (
+    <Fragment>
+      <main className="MainContainer">
+        <Instructions />
+
+        <hr className="RampBreak--l" />
+
+        <InputSelect<Employee>
+          isLoading={isLoading}
+          defaultValue={EMPTY_EMPLOYEE}
+          items={employees === null ? [] : [EMPTY_EMPLOYEE, ...employees]}
+          label="Filter by employee"
+          loadingLabel="Loading employees"
+          parseItem={(item) => ({
+            value: item.id,
+            label: `${item.firstName} ${item.lastName}`,
+          })}
+          onChange={async (newValue) => {
+            if (newValue === null) {
+              return
+            }
+            else if (newValue.firstName === "All" && newValue.lastName === "Employees"){
+              loadAllTransactions();
+             
+            
+            }
+            else{
+              
+              
+              await loadTransactionsByEmployee(newValue.id);
+              
+              
+           
+              
+            }
+          }}
+        />
+
+        <div className="RampBreak--l" />
+
+        <div className="RampGrid">
+          {transactions === null ? (
+            <div className="RampLoading--container">Loading...</div>
+          ) : (
+            <Fragment>
+              <div data-testid="transaction-container">
+                {transactions.map((transaction) => (
+                  <TransactionPane key={transaction.id} transaction={transaction} />
+                ))}
+              </div>
+              <button
+                className="RampButton"
+                disabled={isPaginated || paginatedTransactionsUtils.loading ||  transactionsByEmployee?.length === 0 || paginatedTransactions?.nextPage === null}
+                onClick={async () => {
+                  await loadAllTransactions()
+                
+                }}
+              >
+                View More
+              </button>
+              
+            </Fragment>
+          )}
+        </div>
+      </main>
+    </Fragment>
+  )
+}
 
     await employeeUtils.fetchAll()
     await paginatedTransactionsUtils.fetchAll()
